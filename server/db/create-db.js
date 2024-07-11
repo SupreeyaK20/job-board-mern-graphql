654110014c6ec1b7c9afc8fd042983da73e76ec0
SupreeyaK20 <101087353+SupreeyaK20@users.noreply.github.com>
2024-07-11 18:21:08 +0530

    Data  change

--- server/db/create-db.js
+++ server/db/create-db.js
@@ -32,37 +32,49 @@ await schema.createTable('user', (table) => {
 
 await connection.table('company').insert([
   {
-    id: 'FjcJCHJALA4i',
+    id: 'comp-01',
     name: 'Facegle',
-    description: 'We are a startup on a mission to disrupt social search engines. Think Facebook meet Google.',
+    description: 'We are a startup on a mission of disrupt AI world',
   },
   {
-    id: 'Gu7QW9LcnF5d',
+    id: 'comp-02',
     name: 'Goobook',
-    description: 'We are a startup on a mission to disrupt search social media. Think Google meet Facebook.',
+    description: 'Gopedia - we store the data',
+  },
+  {
+    id: 'comp-03',
+    name: 'Zexo',
+    description: 'New product base startup',
   },
 ]);
 
 await connection.table('job').insert([
   {
     id: 'f3YzmnBZpK0o',
-    companyId: 'FjcJCHJALA4i',
+    companyId: 'comp-01',
     title: 'Frontend Developer',
     description: 'We are looking for a Frontend Developer familiar with React.',
     createdAt: '2023-01-26T11:00:00.000Z',
   },
   {
     id: 'XYZNJMXFax6n',
-    companyId: 'FjcJCHJALA4i',
+    companyId: 'comp-01',
     title: 'Backend Developer',
     description: 'We are looking for a Backend Developer familiar with Node.js and Express.',
     createdAt: '2023-01-27T11:00:00.000Z',
   },
   {
     id: '6mA05AZxvS1R',
-    companyId: 'Gu7QW9LcnF5d',
+    companyId: 'comp-02',
     title: 'Full-Stack Developer',
     description: 'We are looking for a Full-Stack Developer familiar with Node.js, Express, and React.',
+    createdAt: '2023-01-30T11:00:00.000Z',
+  },
+  {
+    id: '6mA05AZxvS1I',
+    companyId: 'comp-03',
+    title: 'Analyst',
+    description: 'We are looking for a Business Analyst.',
     createdAt: '2023-01-30T11:00:00.000Z',
   },
 ]);
@@ -70,14 +82,20 @@ await connection.table('job').insert([
 await connection.table('user').insert([
   {
     id: '1',
-    companyId: 'FjcJCHJALA4i',
+    companyId: 'comp-01',
     email: 'user1@comapny.com',
     password: '12345678',
   },
   {
     id: '2',
-    companyId: 'Gu7QW9LcnF5d',
+    companyId: 'comp-02',
     email: 'user2@comapny.com',
+    password: '12345678',
+  },
+  {
+    id: '3',
+    companyId: 'comp-03',
+    email: 'user3@comapny.com',
     password: '12345678',
   },
 ]);
